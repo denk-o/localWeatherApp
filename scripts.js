942ac9a1add1850c0105665fdad45967a67c8a73
Kevin Liu <kevinliu314@gmail.com>
2016-03-09 17:45:55 -0500

    create an object
    store lat lo ng in object

--- scripts.js
+++ scripts.js
@@ -2,8 +2,7 @@ var apiKey_forecast="/c85793a790b8bfb1e334c7c126839718/";
 var queryString_forecast="https://api.forecast.io/forecast";
 var apiKey_googleGeocode = "AIzaSyAPQEOpIRMn2R3W7SggIh4j1DZp50CX_x0	";
 var queryString_googleGeocode="https://maps.googleapis.com/maps/api/geocode/json?";
-var lat;
-var long;
+var coords_local = {lat:"",long:""};
 var cityString="address=";
 queryString_forecast += apiKey_forecast;
 
@@ -11,9 +10,9 @@ function searchCity(){
   alert("good");
 }
 function getLocalWeather(){//should be a way to consolidate local and non local weather into a single function
-  console.log(lat);
-  console.log(long);
-  queryString_forecast = queryString_forecast+lat+","+long;
+  console.log(coords_local.lat);
+  console.log(coords_local.long);
+  queryString_forecast = queryString_forecast+coords_local.lat+","+coords_local.long;
   $.ajax({
     url:queryString_forecast,
     dataType:"jsonp",
@@ -39,8 +38,8 @@ $(document).ready(function(){
     }
   }
   function savePosition(position){
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
+    coords_local.lat = position.coords.latitude;
+    coords_local.long = position.coords.longitude;
   }
 
   //main body
