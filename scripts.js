6bd5439e6cbc96e132e63def4e4ec90f768e7461
Kevin Liu <kevinliu314@gmail.com>
2016-03-10 01:05:25 -0500

    working prototype
    added weather icons and have a working prototype that fulfills all
    stories, styling to be done and search to be implemented

--- scripts.js
+++ scripts.js
@@ -1,13 +1,46 @@
 var apiKey_forecast="/c85793a790b8bfb1e334c7c126839718/";
 var queryString_forecast="https://api.forecast.io/forecast";
-var apiKey_googleGeocode = "AIzaSyAPQEOpIRMn2R3W7SggIh4j1DZp50CX_x0	";
+var apiKey_googleGeocode = "AIzaSyDq73ibmNV8HFrapwLzhiFhwLsi63uZ-jM	";
 var queryString_googleGeocode="https://maps.googleapis.com/maps/api/geocode/json?";
 var cityString="address=";
 queryString_forecast += apiKey_forecast;
 
+var weather_map={
+  "clear-day":"wi-day-sunny",
+  "clear-night":"wi-night-clear",
+  "rain":"wi-rain",
+  "snow":"wi-snow",
+  "sleet":"wi-sleet",
+  "wind":"wi-windy",
+  "fog":"wi-fog",
+  "cloudy":"wi-cloudy",
+  "partly-cloudy-day":"wi-day-cloudy",
+  "partly-cloudy-night":"wi-night-cloudy"
+};
+
+
 function searchCity(){
   //eventually gonna get this working
+  var city = $("#location_in").val();
+  cityString+=city;
+  queryString_googleGeocode=queryString_googleGeocode+cityString+"&key="+apiKey_googleGeocode;
+  $.ajax({
+    url: queryString_googleGeocode,
+    dataType: "json",
+    success:function(data){
+      console.log(data);
+      alert("success");
+    }
+  });
 }
+
+function getWeatherIcon(icon){
+  if(icon in weather_map){
+    return weather_map[icon];
+  }
+  else return icon in weather_map || (weather_map.icon="wi-day-sleet-storm");
+}
+
 function getWeather(lat, long){//should be a way to consolidate local and non local weather into a single function
   console.log(lat);
   console.log(long);
@@ -17,10 +50,14 @@ function getWeather(lat, long){//should be a way to consolidate local and non lo
     dataType:"jsonp",
     success:function(data){
       console.log(data);
-      var res = data.currently;
-      $("#weather_div").html(res);
+      var temp = data.currently.temperature;
+      var status = data.currently.summary;
+      var weather_icon=getWeatherIcon(data.currently.icon);
+      $("#weather_div").html("The current temperature is: "+temp+"<br/>"+
+        "The current weather is: "+status+"<br/>"+
+        "<i class='wi "+weather_icon+"'></i>");
     }
-  })
+  });
 }
 
 function getSearchWeather(){
@@ -38,7 +75,6 @@ $(document).ready(function(){
   function savePosition(position){
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
-    console.log("save");
     getWeather(lat, long);
   }
   getCurrLocation();
